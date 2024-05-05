5a91ebbe403932560914f7684f671f095219bbec
sonyflavio <sonyflavio@gmail.com>
2024-05-05 14:57:26 -0300

    enviando versão final do projeto

--- Projeto Conta Bancária/Conta-Banco/src/ContaTerminal.java
+++ Projeto Conta Bancária/Conta-Banco/src/ContaTerminal.java
@@ -1,6 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Bem vindo ao banco Devcash");
+        System.out.println();
+        System.out.println("Qual seu nome? ");
+        String clientName = sc.nextLine();
+
+        System.out.println("Por favor, digite o número  da sua Conta! ");
+        int accountNumber = sc.nextInt();
+
+        System.out.println("Agora digite sua Agêcia! ");
+        String agency = sc.next();
+
+        System.out.println("Digite o valor de depósito inicial: ");
+        double initialBalance = sc.nextDouble();
+        
+        System.out.println("Olá " + clientName + " " + ", obrigado por criar uma conta em nosso banco, sua agência é " + agency + " " + ", conta " + accountNumber + " " + "e seu saldo " + "R$"+initialBalance + " " + "já está disponível para saque." );
+        System.out.println();
+        
+        System.out.println("O banco Devcash agradeçe a preferência!");
+        System.out.println("Tenha um ótimo dia.");
+
+
+
+
+        sc.close();
     }
 }
